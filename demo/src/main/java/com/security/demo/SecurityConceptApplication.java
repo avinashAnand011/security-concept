2e5dbbe7724f56a18da4d07e05cac404cde6ef79
avinashAnand011 <anandavinash456@gmmail.com>
2025-08-01 19:52:40 +0530

    getting started with the project

---
+++ demo/src/main/java/com/security/demo/SecurityConceptApplication.java
@@ -0,0 +1,13 @@
+package com.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityConceptApplication.class, args);
+	}
+
+}
